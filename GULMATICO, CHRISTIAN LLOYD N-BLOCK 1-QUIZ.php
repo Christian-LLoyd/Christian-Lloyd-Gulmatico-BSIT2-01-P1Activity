d2e1b3776c5bb0fff9402ca38417baf27d93a2fd
Christian-LLoyd <christianlloydgulmatico@gmail.com>
2023-12-05 23:23:17 +0800

    Create GULMATICO, CHRISTIAN LLOYD N-BLOCK 1-QUIZ.php

---
+++ GULMATICO, CHRISTIAN LLOYD N-BLOCK 1-QUIZ.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+     <label>Raduis:</label><br>
+     <input type = "text" name = "radius"><br>
+     <input type = "submit" value = "calculate">
+
+
+    </form>
+    
+</body>
+</html>
+
+
+<?php
+
+   
+
+
+
+?>
+
